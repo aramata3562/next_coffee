018e3604e89ea9831406dc6dd2b543efd6db97ce
aramakitaketo <aramakitaketo@gmail.com>
2024-05-14 17:57:00 +0900

    chenge config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-      NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
-    },
-  };
-  module.exports = nextConfig
-  export default nextConfig;
-  
+  env: {
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+  },
+};
+
+export default nextConfig;
