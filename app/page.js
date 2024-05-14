5939e7467e0f8a65819955bb290f78aa2d256386
aramakitaketo <aramakitaketo@gmail.com>
2024-05-14 17:47:53 +0900

    main push

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import Link from "next/link"
 import Image from "next/image"
 
 const getAllItems = async() => {
+    const apiUrl = `${process.env.NEXT_PUBLIC_URL}/api/item/readall`;
+    console.log("Fetching from URL:", apiUrl); // 追加: URLをコンソールに出力
   const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/readall`, {cache: "no-store"})
   const jsonData = await response.json()
   const allItems = jsonData.allItems
